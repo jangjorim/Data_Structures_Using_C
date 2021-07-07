96549e292e436af8290463215241921414c8e0df
jangjorim <nims8006@gmail.com>
2021-07-07 20:59:55 +0900

    연결 리스트02( 연결 리스트 코드를  응용하여 데이터 저장 순서가 역순으로 저장되게 조금 수정하였다.)

---
+++ Linked_List_02/Linked_Read_Contrary/LinkedReadContrary.c
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node {			// _node 라는 이름의 구조체를 형성
+	int data;			// 노드의 데이터를 저장하는 변수
+	struct _node* next;			// 다음 노드의 주소를 저장하는 포인터 변수
+} Node;			// typedef로 Node라는 이름으로 구조체 이름을 재설정 한다.
+
+int main() {
+	Node* head = NULL;			// 연결리스트의 머리를 가리키는 포인터 주소를 담는 변수
+	Node* tail = NULL;			// 연결리스트의 꼬리를 가리키는 포인터 주소를 담는 변수
+	Node* cur = NULL;			// 연결리스트의 저장된 데이터를 조회하는데 사용되는 변수
+
+	Node* newNode = NULL;			// 새로운 노드의 주소를 담는 포인터 변수
+	int readData;			// 입력받는 데이터를 저장하는 변수
+
+	// 데이터를 입력 받는 과정 //
+	printf("0 이하의 수를 입력시 입력이 종료됩니다. \n");
+	while (1) {
+		printf("자연수 입력: ");
+		scanf_s("%d", &readData);			// 수를 입력받기
+		if (readData < 1)			// 0이하의 수를 입력하면 데이터를 그만 입력받는다.
+			break;
+
+		// 노드의 추가과정
+		newNode = (Node*)malloc(sizeof(Node));			// 새로운 노드를 할당한다.
+		newNode->data = readData;			// 새노드 데이터에 입력 받은 값을 저장한다.
+		newNode->next = NULL;			// 새노드 다음에 이어지는(연결해야하는) 곳의 주소는 비워놓는다.
+
+
+		// 아래 구문을 통해서 데이터를 1,2,3 이렇게 저장된 방식이 -> 3,2,1 이렇게 저장된다.
+		if (head == NULL) {			// 노드가 비어져 있다면
+			head = newNode;			// 헤드가 새 노드의 주소를 가리키게 한다.
+			tail = newNode;			// 꼬리도 새 노드의 주소를 가리키게 한다.
+		}			
+		else {			// 노드에 데이터가 들어 있다면
+			newNode->next = head;			// 노드의 주소부분이 헤드를 가리키게 한다.
+			head = newNode;			// 새 노드의 주소를 다시 헤드부분에 넣어 준다.
+			// 위와 같이 작성하게 되면 새 노드는 항상 head를 가리키고 먼저 들어온 노드는 새 노드를 가리키게 된다.
+			// 스택처럼 나중에 저장된 값부터 출려되고 먼저 저장된 값이 제일 나중에 출력되는 것이다.
+		}
+	}
+	printf("\n");
+
+	// 입력 받은 데이터의 출력과정 //
+	printf("입력 받은 데이터의 전체출력! \n");
+	if (head == NULL) {			// 연결리스트가 시작되는 해드부분이 없으면 저장된 데이터가 없다는 뜻이다.
+		printf("저장된 자연수가 존재하지 않습니다. \n");
+	}
+	else {
+		cur = head;			// 탐색에 사용되는 포인터 변수에 리스트의 처음 시작되는 주소를 대입한다.
+		printf("%d ", cur->data);			// 첫 번째 데이터 출력
+
+		while (cur->next != NULL) {			// 두 번째 이후의 데이터가 있다면 마지막 노드까지 실행한다.
+			cur = cur->next;			// 데이터 출력을 하고난 다음 노드의 주소값을 대입받는다.
+			printf("%d ", cur->data);			// 다음노드의 데이터 값을 출력한다.
+		}
+	}
+	printf("\n\n");
+
+	// 메모리 해제과정 //
+	if (head == NULL) {
+		return 0;			// 해제할 노드가 존재하지 않는다.
+	}
+	else {
+		Node* delNode = head;			// 삭제할 노드의 주소값을 저장할 포인터 변수에 연결리스트의 헤드 주소 값을 넣는다.
+		Node* delNextNode = head->next;			// 삭제할 노드의 다음 주소값을 저장하는 포인터 변수를 선언하고 헤드의 next값을 넣는다.
+
+		printf("%d을(를) 삭제합니다. \n", head->data);			// 삭제할 데이터를 화면에 출력해 준다.
+		free(delNode);			// 첫 번째 노드 삭제하여 메모리 주소 할당을 해지한다.
+
+		while (delNextNode != NULL) {			// 다음 노드가 있다면 계속 실행한다.
+			delNode = delNextNode;			// 삭제후 다음에 삭제할 데이터가 있는 다음 주소값을 삭제할 노드를 가리키는 포인터 변수에 넣는다.
+			delNextNode = delNextNode->next;			// 삭제하는 데이터가 있는 다음 주소값을 넣는다.
+
+			printf("%d을(를) 삭제합니다. \n", delNode->data);				// 삭제하는 데이터를 출력해 준다.
+			free(delNode);			// 메모리 할당을 해지해 준다.
+		}
+	}
+	return 0;
+}

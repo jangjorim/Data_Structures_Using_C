49df6f796640e04acc4bab670f2aaad8441e042a
jangjorim <nims8006@gmail.com>
2021-07-20 11:21:55 +0900

    연결 리스트03(원형 연결 리스트 코드를 분석하여 전체 주석문을 작성하였다.)

--- Linked_List_03/Circular_Linked_List/CLinkedList.c
+++ Linked_List_03/Circular_Linked_List/CLinkedList.c
@@ -2,85 +2,86 @@
 #include <stdlib.h>
 #include "CLinkedList.h"
 
-void ListInit(List* plist) {
-	plist->tail = NULL;
+void ListInit(List* plist) {			// 리스트를 초기화하는 함수
+	plist->tail = NULL;					
 	plist->cur = NULL;
 	plist->before = NULL;
 	plist->numOfData = 0;
 }
 
-void LInsertFront(List* plist, Data data) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
+void LInsertFront(List* plist, Data data) {			// 리스트 머리에 노드를 추가하는 함수
+	Node* newNode = (Node*)malloc(sizeof(Node));			// 메모리 공간을 할당 받아 새 노드를 생성
+	newNode->data = data;			// 새 노드에 데이터를 삽입
 
-	if (plist->tail == NULL) {
-		plist->tail = newNode;
-		newNode->next = newNode;
+	if (plist->tail == NULL) {			// 꼬리 노드가 비어있다면
+		plist->tail = newNode;			// 꼬리 노드를 새 노드로 한다.
+		newNode->next = newNode;		// 다음 노드 주소를 연결을 자신으로 한다.
 	}
-	else {
-		newNode->next = plist->tail->next;
-		plist->tail->next = newNode;
+	else {			// 꼬리 노드가 있다면
+		newNode->next = plist->tail->next;			// 새 노드 next에 꼬리 노드의 next 주소를 넣는다.
+		plist->tail->next = newNode;				// 꼬리 노드 next에 새 노드 주소값을 넣는다.
 	}
-	(plist->numOfData)++;
+	(plist->numOfData)++;			// 저장된 노드 갯수를 +1 해준다.
 }
 
-void LInsert(List* plist, Data data) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = data;
+void LInsert(List* plist, Data data) {			// 리스트 꼬리에 노드를 추가하는 함수
+	Node* newNode = (Node*)malloc(sizeof(Node));			// 메모리 공간을 할당 받아 새 노드를 생성
+	newNode->data = data;			// 새 노드에 데이터를 삽입
 
-	if (plist->tail == NULL) {
-		plist->tail = newNode;
-		newNode->next = newNode;
+	if (plist->tail == NULL) {			// 꼬리 노드가 비어있다면
+		plist->tail = newNode;			// 꼬리 노드를 새 노드로 한다.
+		newNode->next = newNode;		// 다음 노드 주소를 연결을 자신으로 한다.
 	}
-	else {
-		newNode->next = plist->tail->next;
-		plist->tail->next = newNode;
-		plist->tail = newNode;
-		(plist->numOfData)++;
+	else {			// 꼬리 노드가 있다면
+		newNode->next = plist->tail->next;			// 새 노드 next에 꼬리 노드의 next 주소를 넣는다.
+		plist->tail->next = newNode;				// 꼬리 노드 next에 새 노드 주소값을 넣는다.
+		plist->tail = newNode;						// 꼬리 노드를 새 노드로 설정한다.
 	}
+	(plist->numOfData)++;			// 저장된 노드 갯수를 +1 해준다.
 }
 
-int LFirst(List* plist, Data* pdata) {
-	if (plist->tail == NULL)
-		return FALSE;
+int LFirst(List* plist, Data* pdata) {			// 리스트 탐색시 처음에 실행되는 함수
+	if (plist->tail == NULL)			// 노드가 존재하지 않는다면
+		return FALSE;					// 0을 반환한다.
 
-	plist->before = plist->tail;
-	plist->cur = plist->tail->next;
-	
-	*pdata = plist->cur->data;
+	plist->before = plist->tail;		// before가 꼬리를 가리키게 하고
+	plist->cur = plist->tail->next;		// cur이 머리를 가리키게 한다.
+	// 결국 before와 cur은 전부 tail노드를 가리키고있다. //
+
+	*pdata = plist->cur->data;			// cur이 가리키는 노드의 데이터를 반환한다.
 	return TRUE;
 }
 
-int LNext(List* plist, Data* pdata) {
-	if (plist->tail == NULL)
-		return FALSE;
+int LNext(List* plist, Data* pdata) {			// 리스트 탐색시 LFirst 이후로 실행되는 함수
+	if (plist->tail == NULL)			// 노드가 존재하지 않는다면
+		return FALSE;					// 0을 반환한다.
 
-	plist->before = plist->cur;
-	plist->cur = plist->cur->next;
+	plist->before = plist->cur;			// before가 다음 노드를 가리키게 한다.
+	plist->cur = plist->cur->next;		// cur이 다음 노드를 가리키게 한다.
 
-	*pdata = plist->cur->data;
+	*pdata = plist->cur->data;			// cur이 가리키는 노드의 데이터를 반환한다.
 	return TRUE;
 }
 
-Data LRemove(List* plist) {
-	Node* rpos = plist->cur;
-	Data rdata = rpos->data;
+Data LRemove(List* plist) {				// 노드를 삭제하는 함수
+	Node* rpos = plist->cur;			// rpos이 cur을 가리키게 한다.
+	Data rdata = rpos->data;			// rdata에 rpos의 데이터를 대입한다.
 
-	if (rpos == plist->tail) {
-		if (plist->tail = plist->tail->next)
-			plist->tail = NULL;
-		else
-			plist->tail = plist->before;
+	if (rpos == plist->tail) {			// 삭제 대상을 tail이 가리킨다면
+		if (plist->tail == plist->tail->next)			// 마지막 남은 노드라면
+			plist->tail = NULL;			// tail 노드를 NULL로 초기화 한다.
+		else							// tail 이외에 다른 노드가 더 있다면
+			plist->tail = plist->before;			// tail이 before를 가리키게 한다.
 	}
 
-	plist->before->next = plist->cur->next;
-	plist->cur = plist->before;
+	plist->before->next = plist->cur->next;			// before와 cur이 가리키는 노드를 한칸씩 이동한다.
+ 	plist->cur = plist->before;						// cur이 before를 가리키게 한다.
 
-	free(rpos);
-	(plist->numOfData)--;
-	return rdata;
+	free(rpos);			// 메모리 할당을 해지한다.
+	(plist->numOfData)--;			// 저장된 노드의 갯수를 -1한다.
+	return rdata;					// 삭제된 데이터 값을 반환한다.
 }
 
-int LCount(List* plist) {
-	return plist->numOfData;
+int LCount(List* plist) {			// 현재 저장된 노드의 갯수를 반환하는 함수
+	return plist->numOfData;		
 }
--- Linked_List_03/Circular_Linked_List/CLinkedList.h
+++ Linked_List_03/Circular_Linked_List/CLinkedList.h
@@ -1,33 +1,33 @@
 #pragma once
-#ifndef __C_LINKED_LIST_H__
+#ifndef __C_LINKED_LIST_H__			// 재 선언이 되지 않도록 제한을 주는 구문
 #define __C_LINKED_LIST_H__
 
-#define TRUE	1
+#define TRUE	1			// 매크로 선언 
 #define FALSE	0
 
-typedef int Data;
+typedef int Data;			// int 형을 Data로 선언
 
-typedef struct _node {
-	Data data;
-	struct _node* next;
-}Node;
+typedef struct _node {			// 노드 구조체를 선언
+	Data data;			// Data형 데이터를 담는 data 변수 선언
+	struct _node* next;			// 다음 노드의 주소를 가리키는 _node형 포인터 변수 선언
+}Node;			// 구조체 이름을 Node로 재선언
 
-typedef struct _CLL {
-	Node* tail;
-	Node* cur;
-	Node* before;
-	int numOfData;
-} CList;
+typedef struct _CLL {			// 연형 연결 리스트를 구성하는 구조체를 선언
+	Node* tail;			// 리스트의 꼬리를 가리키는 노드
+	Node* cur;			// 현재 노드를 가리키고 있는 노드
+	Node* before;		// 현재 노드에서 한단계 이전의 노드를 가리키는 노드
+	int numOfData;		// 저장된 노드의 갯수를 저장하는 변수
+} CList;				// 구조체 이름을 CList로 재선언
 
-typedef CList List;
+typedef CList List;			// 구조체 이름을 List로 재 선언
 
-void ListInit(List* plist);
-void LInsert(List* plist, Data data);
-void LInsertFront(List* plist, Data data);
+void ListInit(List* plist);			// 리스트를 초기화 하는 함수
+void LInsert(List* plist, Data data);			// 노드를 꼬리에 추가하는 함수
+void LInsertFront(List* plist, Data data);		// 노드를 머리에 추가하는 함수
 
-int LFirst(List* plist, Data* pdata);
-int LNext(List* plist, Data* pdata);
-Data LRemove(List* plist);
-int LCount(List* plist);
+int LFirst(List* plist, Data* pdata);			// 리스트 탐색시 처음에만 실행되는 함수
+int LNext(List* plist, Data* pdata);			// 리스트 탐색시 LFirst 다음으로 실행하는 함수
+Data LRemove(List* plist);						// 현재 가리키고 있는 노드를 삭제하는 함수
+int LCount(List* plist);						// 현재 리스트에 저장된 노드의 갯수를 반환 하는 함수
 
 #endif // !__C_LINKED_LIST_H__

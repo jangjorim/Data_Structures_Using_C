c90eb1fd874ea0177f29c70482dd55af61223abd
jangjorim <48784521+jangjorim@users.noreply.github.com>
2021-08-01 22:53:05 +0900

    Update ListBaseStack.c

--- Stack/Linked_List_Base_Stack/ListBaseStack.c
+++ Stack/Linked_List_Base_Stack/ListBaseStack.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "ListBaseStack.h"
 
-void StackInit(Stack* pstack) {
+void StackInit(Stack* pstack) {			// 스택 초기화 함수 정의
 	pstack->head = NULL;
 }
 
-int SIsEmpty(Stack* pstack) {
+int SIsEmpty(Stack* pstack) {			// 스택이 공백일 때 작동하는 함수 정의
 	if (pstack->head == NULL) {
 		return TRUE;
 	}
@@ -15,13 +15,13 @@ int SIsEmpty(Stack* pstack) {
 	}
 }
 
-void SPush(Stack* pstack, Data data) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
+void SPush(Stack* pstack, Data data) {			// 스택에 값을 삽입하는 함수 정의
+	Node* newNode = (Node*)malloc(sizeof(Node));			// 새로운 노드 생생
 
-	newNode->data = data;
-	newNode->next = pstack->head;
+	newNode->data = data;						// 새노드에 데이터 삽입
+	newNode->next = pstack->head;					// 헤드가 가리키는 노드를 새노드가 가리킨다
 
-	pstack->head = newNode;
+	pstack->head = newNode;						//헤드가 새노드를 가리키게 하여 머리부분에 노드를 추가 시킨다.
 }
 
 Data SPop(Stack* pstack) {

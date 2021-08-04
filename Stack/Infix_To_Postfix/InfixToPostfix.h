30722ffa9048ea8a3d96d0cfea469ffc0fdec487
jangjorim <48784521+jangjorim@users.noreply.github.com>
2021-08-04 21:10:29 +0900

    Update InfixToPostfix.h

--- Stack/Infix_To_Postfix/InfixToPostfix.h
+++ Stack/Infix_To_Postfix/InfixToPostfix.h
@@ -1,6 +1,6 @@
 #ifndef __INRIX_TO_POSTFIX_H__
-#define __INRIX_TO_POSTFIX_H__
+#define __INRIX_TO_POSTFIX_H__      //  중복 선언을 막기위해 작성된 코드
 
-void ConvToRPNExp(char exp[]);
+void ConvToRPNExp(char exp[]);      // 중기 표기를 후위표기식으로 변환하는 함수 선언
 
 #endif // !__INRIX_TO_POSTFIX_H__

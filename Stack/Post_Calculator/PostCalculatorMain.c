83dfd1a1b5f258e6097e6fa86833ba166432afe0
jangjorim <48784521+jangjorim@users.noreply.github.com>
2021-08-17 23:50:32 +0900

    Update PostCalculatorMain.c

--- Stack/Post_Calculator/PostCalculatorMain.c
+++ Stack/Post_Calculator/PostCalculatorMain.c
@@ -2,10 +2,10 @@
 #include "PostCalculator.h"
 
 int main() {
-	char postExp1[] = "42*8+";
+	char postExp1[] = "42*8+";            // 배열에 후위식을 삽입한다.
 	char postExp2[] = "123+*4/";
 
-	printf("%s = %d \n", postExp1, EvalRPNExp(postExp1));
+	printf("%s = %d \n", postExp1, EvalRPNExp(postExp1));            // 변환한 식을 화면에 출력한다.
 	printf("%s = %d \n", postExp2, EvalRPNExp(postExp2));
 
 	return 0;

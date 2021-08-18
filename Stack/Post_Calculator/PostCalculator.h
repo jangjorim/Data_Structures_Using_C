dc984f7f3d4bc11fb62d9c8063039f83d5517ed6
jangjorim <48784521+jangjorim@users.noreply.github.com>
2021-08-18 19:59:13 +0900

    Update PostCalculator.h

--- Stack/Post_Calculator/PostCalculator.h
+++ Stack/Post_Calculator/PostCalculator.h
@@ -1,6 +1,6 @@
 #ifndef __POST_CALCULATOR_H__
-#define __POST_CALCULATOR_H__
+#define __POST_CALCULATOR_H__     // 중복 선언이 안되게 작성하는 코드
 
-int EvalRPNExp(char exp[]);
+int EvalRPNExp(char exp[]);     // 후위식을 계산하는 함수 선언
 
 #endif // !__POST_CALCULATOR_H__
